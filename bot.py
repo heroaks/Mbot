5b740e4e0c7c2440c60b9da00d7ed7ccc1227ffb
heroaks <ryuga642002@gmail.com>
2023-08-12 15:15:56 +0530

    Files Initialized

---
+++ bot.py
@@ -0,0 +1,49 @@
+import discord
+import responses
+from discord.ext import commands
+import music
+from dotenv import loead_dotenv
+
+load_dotenv()
+
+async def send_message(message, user_message, is_private):
+    try:
+        response = responses.get_response(user_message)
+        await message.author.send(response) if is_private else await message.channel.send(response)
+
+    except Exception as e:
+        print(e)
+
+
+def run_discord_bot():
+
+
+    client = commands.Bot(command_prefix='?', intents=discord.Intents.all())
+
+    cogs = [music]
+
+    for i in range(len(cogs)):
+        cogs[i].setup(client)
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now running!')
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+
+        print(f'{username} said: "{user_message}" ({channel})')
+
+        if user_message[0] == '?':
+            user_message = user_message[1:]
+            await send_message(message, user_message, is_private=True)
+        else:
+            await send_message(message, user_message, is_private=False)
+
+    client.run(TOKEN)
